1880109606b8e7d9c6484792fc69c1307b2e1183
jacky <785340449.qq.com>
2019-11-07 15:38:37 +0800

    第一次提交项目源码

---
+++ src/main/java/com/jacky/W3CSpringSource/App.java
@@ -0,0 +1,13 @@
+package com.jacky.W3CSpringSource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
